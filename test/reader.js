da5c53f33705cf7912edeca6f238dd592dc4f3e7
ysden123 <ysden123@gmail.com>
2016-03-21 23:45:11 +0200

    module exports

--- test/reader.js
+++ test/reader.js
@@ -1,24 +1,8 @@
 // Test for reader.
 'use strict';
-// var csvReader = require('../lib');
-// var reader = csvReader.fileReader;
 
-function lineHandler(line){
-   console.log(line); 
-}
+var csvReader = require('..');
 
-// console.log(csvReader);
-// console.log(reader);
-
-// var fileReader = reader.fileReader('test1.csv', lineHandler);
-
-// fileReader();
-
-var reader = require('../lib/reader');
-console.log(reader);
-console.log(reader.fileReader);
-
-// var fileReader = reader.fileReader('test1.csv', lineHandler);
-var fileReader = reader.fileReader('test1.csv', (line)=>{
+var fileReader = csvReader.fileReader('test1.csv', (line) => {
     console.log(line);
 });
