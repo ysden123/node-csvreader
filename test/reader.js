e6e8bc1907d030058e1707efe6bc617ec69fa87e
ysden123 <ysden123@gmail.com>
2016-03-24 22:46:51 +0200

    skip
    Parameter options was added. Attribute "skip" is supported.

--- test/reader.js
+++ test/reader.js
@@ -3,6 +3,12 @@
 
 var csvReader = require('..');
 
-var fileReader = csvReader.reader('test1.csv', (record) => {
+// var fileReader = csvReader.reader('test1.csv', (record) => {
+//     console.log(record);
+// });
+
+// console.log();
+
+var fileReaderWithSkip = csvReader.reader('test1.csv', (record) => {
     console.log(record);
-});
+}, {skip: 5});
